b020997cbb1ed2b0cf9f9c9ddf39ebbe46129cb4
Ekta Sinha <youremail@example.com>
2025-12-29 11:25:05 +0530

    Initial commit - frontend

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import './footer.css'
+import { assets } from '../../assets/assets'
+
+const Footer = () => {
+  return (
+    <div className='footer' id='footer'>
+        <div className="footer-content">
+
+            <div className="footer-content-left">
+                <img src={assets.logo} alt=''/>
+                <p>FoodDel is a modern food delivery platform offering a wide variety of dishes from top restaurants in your area. We aim to deliver fresh, delicious meals quickly and reliably. Whether you're craving healthy salads, comfort food, or gourmet meals, FoodDel has something for everyone.</p>
+                <div className="footer-social-icons">
+                    <img src={assets.facebook_icon} alt=''/>
+                    <img src={assets.twitter_icon} alt=''/>
+                    <img src={assets.linkedin_icon} alt=''/>
+                </div>
+
+            </div>
+            <div className="footer-content-center">
+                <h2>COMPANY</h2>
+                <ul>
+
+                    <li>Home</li>
+                    <li>About us</li>
+                    <li>Delivery</li>
+                    <li>Privacy policy</li>
+                
+                </ul>
+            </div>
+
+            <div className="footer-content-right">
+                <h2>GET IN TOUCH</h2>
+                <ul>
+                    <li>+1-212-456-7890</li>
+                    <li>contact@tomato.com</li>
+                </ul>
+            </div>
+
+        </div>
+        <hr/>
+        <p className="footer-copyright">Copyright 2025 © Tomato.com - All Right Reserved. </p>
+    </div>
+  )
+}
+
+export default Footer
